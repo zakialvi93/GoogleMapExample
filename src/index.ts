311ff249a331d7da6d083f309bfc47070313bf59
zakialvi-e64 <zaki.alvi@enterprise64.com>
2023-08-21 10:16:53 +0500

    script for index.ts added in index.html file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { User } from "./User";
 import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
+import env from 'dotenv';
+
+env.config();
 
 const user = new User();
 const company = new Company();
 const customMap = new CustomMap('map');
+
+export const apiKey = process.env.API_KEY;
+console.log(apiKey);
 
 customMap.addMarker(user);
 customMap.addMarker(company);
