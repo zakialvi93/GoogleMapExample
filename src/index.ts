1bd266c5c2281db6cf4fe078a96621b3578c8c69
zakialvi-e64 <zaki.alvi@enterprise64.com>
2023-08-21 10:22:03 +0500

    wait again

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { User } from "./User";
 import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
+import env from 'dotenv';
+
+env.config();
+
+export const apiKey = process.env.API_KEY;
+console.log(apiKey);
 
 const user = new User();
 const company = new Company();
